ede81f5bb5d7cc824fd50c7227c454531f00f631
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-18 14:28:06 -0500

    removed test

--- src/test/kotlin/org/eten/datasetapiiso6392/ApiServiceTemplateApplicationTests.kt
+++ src/test/kotlin/org/eten/datasetapiiso6392/ApiServiceTemplateApplicationTests.kt
@@ -3,11 +3,11 @@ package org.eten.datasetapiiso6392
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class ApiServiceTemplateApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ApiServiceTemplateApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
